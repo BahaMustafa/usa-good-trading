fbe4996e9d42fe057897d8978993f0c90a97ff57
BahaMustafa <bhaatobase93@gmail.com>
2025-05-19 01:19:27 -0700

    another fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // src/app/layout.tsx
 import './globals.css';
 import { ReactNode } from 'react';
+import Script from 'next/script';
 import Navigation from '@/components/Navigation';
 import AppTransition from '@/components/AppTransition';
 import Footer from '@/components/Footer';
@@ -16,6 +17,19 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Navigation />
         <AppTransition>{children}</AppTransition>
         <Footer />
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "rlwfspw6av");
+            `,
+          }}
+        />
       </body>
     </html>
   );
